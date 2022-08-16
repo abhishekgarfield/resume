cf4ff4e261b651f44d87bf0e68bcd398d98414d1
abhishekgarfield <abhishek2759@gmail.com>
2022-08-16 20:39:57 +0530

    resume

---
+++ resume.js
@@ -0,0 +1,10 @@
+            function change()
+                {
+                    document.getElementById("ic").style.display= 'none';
+                    document.getElementById("io").style.display= "block";
+                }
+            function opn()
+                {
+                        document.getElementById("io").style.display= 'none';
+                        document.getElementById("ic").style.display= "block";
+                }
